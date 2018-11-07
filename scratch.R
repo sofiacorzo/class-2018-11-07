94f95b12766b2c55808f22539ca39b6fd0617e90
Sofia Corzo <corzo@college.harvard.edu>
2018-11-07 14:10:46 -0500

    Initial version of scratch file worked in class

--- scratch.R
+++ scratch.R
@@ -7,19 +7,30 @@
 library(tidyverse)
 library(stringr)
 library(fs)
+library(readr)
 
 # 1. Read data/ex_926_I.csv into a tibble and provide a summary.
 
+data <- read_csv("data/ex_926_I.csv")
+
 # 2. Create a vector with all the file names in data/.
 
+filenames <- dir_ls(path = "data/")
+
 # 3. Create a vector with just the file names that have an "A" in them.
+
+str_subset(filenames, "A")
 
 # 4. Read in all the files into one big tibble. Check out ?map_dfr . . .
 # Background reading here:
 # https://r4ds.had.co.nz/iteration.html#the-map-functions
 
+table <- map_dfr(filenames, read_csv)
+
 # 5. Read in everything and also add a new variable, source, which records the
 # file name from which the data came.
+
+newvar <- map_dfr(file_names, read_csv, .id = "source")
 
 # 6. Find the 4 files with the largest number of observations.
 
